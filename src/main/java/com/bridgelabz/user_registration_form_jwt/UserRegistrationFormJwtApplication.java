b99c291ae4a6de032596cfc3b46644d942a2b82a
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-15 22:58:10 +0530

    Added:Generate JWT Token for Registration and Login Process in User:

---
+++ src/main/java/com/bridgelabz/user_registration_form_jwt/UserRegistrationFormJwtApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.user_registration_form_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationFormJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationFormJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/user_registration_form_jwt/UserRegistrationFormJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.user_registration_form_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationFormJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
